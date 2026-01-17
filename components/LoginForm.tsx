bfb2cb31913e59b582c5308e5169829c03d18b33
raaaton <pranolouis@gmail.com>
2026-01-17 20:39:35 +0100

    feat: made google oauth work

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -10,12 +10,18 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import { Field, FieldDescription, FieldGroup, FieldSeparator } from "@/components/ui/field";
+import {
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldSeparator,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import GoogleAuthButton from "@/components/GoogleAuthButton";
 
 export function LoginForm({
     className,
@@ -47,7 +53,7 @@ export function LoginForm({
             setError(
                 error instanceof Error
                     ? error.message
-                    : "Une erreur est survenue"
+                    : "Une erreur est survenue",
             );
         } finally {
             setIsLoading(false);
@@ -58,7 +64,9 @@ export function LoginForm({
         <div className={cn("flex flex-col gap-6", className)} {...props}>
             <Card>
                 <CardHeader className="text-center">
-                    <CardTitle className="text-xl">Ravi de vous revoir</CardTitle>
+                    <CardTitle className="text-xl">
+                        Ravi de vous revoir
+                    </CardTitle>
                     <CardDescription>
                         Connectez-vous avec votre compte Google
                     </CardDescription>
@@ -66,20 +74,7 @@ export function LoginForm({
                 <CardContent>
                     <form onSubmit={handleLogin}>
                         <FieldGroup>
-                            <Field>
-                                <Button variant="outline" type="button">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
-                                    Se connecter avec Google
-                                </Button>
-                            </Field>
+                            <GoogleAuthButton />
                             <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
                                 Ou continuez avec
                             </FieldSeparator>
